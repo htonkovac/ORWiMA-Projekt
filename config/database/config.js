437280059bd0422b90d84e007b895e0fa7bec0ae
Hrvoje <htonkovac@gmail.com>
2018-07-30 18:52:23 +0200

    updated env var names

--- config/database/config.js
+++ config/database/config.js
@@ -5,7 +5,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_NAME,
-  host: process.env.DB_URL,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 
   operatorsAliases: true,
@@ -15,7 +15,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
-    host: process.env.DB_URL,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 
   }
